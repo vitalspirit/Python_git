b2e15946870bf9fe61542c0289f531f11aa9ef86
Vitalspirit <vitaliymolchanov@mail.ru>
2020-11-11 18:51:16 +0600

    Lesson 1 - homework first half

--- hello.py
+++ hello.py
@@ -1,2 +1,24 @@
-a = 123
-b = 321
+#Задание 1
+
+#a = 5
+#b = "string"
+#c = 3.14
+#d = input("Please enter some integer")
+#e = a + int(d)
+#print(e)
+#f = input("Как Вас Зовут?")
+#g = input("Сколько Вам лет?")
+#print(f'Дорогой {f} вам уже {g} лет!')
+
+#Задание 2
+#timeinseconds = int(input("Enter time in seconds"))
+#hours = timeinseconds//3600
+#minutes = (timeinseconds%3600)//60
+#seconds = (timeinseconds%3600)%60
+#print(f"{hours}:{minutes}:{seconds}")
+
+#Задание 3
+#n = input("Please enter n number")
+#n_sum = int(n) + int(n*2) + int(n*3)
+#print(n_sum)
+
